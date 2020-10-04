62470311e7d0012fd0238f44cb5b2ff86ac365a1
Crowdbotics <team@crowdbotics.com>
2020-10-04 03:19:14 +0000

    Update models

--- backend/vbar_21048/wsgi.py
+++ backend/vbar_21048/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vbar_21048.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vbar_21048.settings")
 
 application = get_wsgi_application()
